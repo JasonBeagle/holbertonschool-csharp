f98a115487b60a92443479b16fafd9a18ab0df72
Jason Beagle <skipskipperson385@gmail.com>
2023-05-12 17:53:35 -0500

    structures task2

---
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        if (index > array.Length || index < 0)
+        {
+            Console.WriteLine("Index out of range");
+            return array;
+        }
+        int[] NewArray = array;
+        for (int i = 0; i < NewArray.Length; i++)
+        {
+            if (index == i)
+            {
+                NewArray[i] = n;
+            }
+        }
+        return NewArray;
+    }
+}
