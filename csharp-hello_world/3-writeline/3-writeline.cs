407e573be52a7a11f2eb8782f6efa8dc5083f7c0
Jason Beagle <skipskipperson385@gmail.com>
2023-05-12 10:45:30 -0500

    hello,world_task3

---
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 3-writeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        }
+    }
+}
