76264de29eb43718774f325281b50e992e09fca6
Jason Beagle <skipskipperson385@gmail.com>
2023-05-12 15:32:06 -0500

    task8

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int last_diggy = Math.Abs(number % 10);
+        Console.Write(last_diggy);
+        return last_diggy;
+    }
+}
