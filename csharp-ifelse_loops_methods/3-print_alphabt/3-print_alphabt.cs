4783fb847f12fb35b50bdf7e3dfce8eed89924ae
Jason Beagle <skipskipperson385@gmail.com>
2023-05-12 13:54:49 -0500

    task3

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(char i = 'a'; i <= 'z'; i++)
+        {
+            if (i =='q' || i =='e')
+                continue;
+            Console.Write(i);
+        }
+    }
+}
