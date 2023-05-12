9b1f55e3ea531060a4223f7b5bc90922c0f93025
Jason Beagle <skipskipperson385@gmail.com>
2023-05-12 17:31:50 -0500

    structures task0

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] NewArray = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            NewArray[i] = i;
+            if (i < size -1)
+                Console.Write(NewArray[i] + " ");
+            else
+                Console.Write(NewArray[i]);
+        }
+        Console.WriteLine();
+        return NewArray;
+    }
+}
