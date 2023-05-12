3090d4f250a6fa7b9fccd975dbef0b349d4505a5
Jason Beagle <skipskipperson385@gmail.com>
2023-05-12 10:28:17 -0500

    task2,correction

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -2,6 +2,6 @@
 # a bash script that initializes and builds a new C# project
 
 dotnet new console -n 2-run_new_project
-cd 1-new_project
-dotnet build
-dotnet run
+cd 2-run_new_project
+dotnet build 2-run_new_project
+dotnet run 2-run_new_project
