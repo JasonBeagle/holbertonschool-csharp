f50ab7d0a9aeb3bf37544010fc5c99b4528fa5ba
Jason Beagle <skipskipperson385@gmail.com>
2023-05-12 11:13:03 -0500

    hello,world task4 too many lines

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -5,7 +5,6 @@ class Program
 	static void Main(string[] args)
 	{
 		int number = 972;
-        string miss = "Mission Street";
-        Console.WriteLine(string.Format("{0} {1}", number, miss));
+        Console.WriteLine(number + " Mission Street");
 	}
 }
