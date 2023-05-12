a41a3e381ec13e0a543e1619019b82a8ae059bed
Jason Beagle <skipskipperson385@gmail.com>
2023-05-12 11:08:35 -0500

    hello,world task4

---
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int number = 972;
+        string miss = "Mission Street";
+        Console.WriteLine(string.Format("{0} {1}", number, miss));
+	}
+}
