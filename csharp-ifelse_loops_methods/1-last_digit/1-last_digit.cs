63a60d15793c21f044d4b0503b82a325eacc07b8
Jason Beagle <skipskipperson385@gmail.com>
2023-05-12 13:14:41 -0500

    ifelse_loops task1

---
+++ csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,17 @@
+﻿﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int ones = number % 10;
+        if (ones > 5)
+            Console.WriteLine("The last digit of {0} is {1} and is greater than 5", number, ones);
+        else if (ones == 0)
+            Console.WriteLine("The last digit of {0} is {1} and is 0", number, ones);
+        else
+            Console.WriteLine("The last digit of {0} is {1} and is less than 6 and not 0", number, ones);
+    }
+}
