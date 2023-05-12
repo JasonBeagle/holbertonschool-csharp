4166550c4d065a306882b43b8e3c087ed8c3c842
Jason Beagle <skipskipperson385@gmail.com>
2023-05-12 13:21:44 -0500

    ifelse_loops task2

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 97;
+
+        while (i <= 122)
+        {
+            Console.Write((char)i);
+            i++;
+        }
+    }
+}
