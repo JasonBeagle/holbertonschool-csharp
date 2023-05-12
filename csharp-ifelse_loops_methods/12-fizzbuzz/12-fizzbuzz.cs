2ae127ce3f674e17d5c27859a417ef6c43ad4828
Jason Beagle <skipskipperson385@gmail.com>
2023-05-12 16:37:53 -0500

    FIZZBUZZ

---
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class FizzBuzz
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.Write("FizzBuzz ");
+                continue;
+            }
+            if (i % 3 == 0)
+            {
+                Console.Write("Fizz ");
+                continue;
+            }
+            if (i % 5 == 0)
+            {
+                Console.Write("Buzz ");
+                continue;
+            }
+            Console.Write(i + " ");
+        }
+    }
+}
