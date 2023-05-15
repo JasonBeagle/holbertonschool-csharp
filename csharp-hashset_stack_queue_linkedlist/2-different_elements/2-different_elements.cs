320dfe8a8c7793697f63220f00f83e0221ce7819
Jason Beagle <skipskipperson385@gmail.com>
2023-05-15 16:03:45 -0500

    task2

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> uniqElem = new List<int>();
+
+        foreach (int element in list1)
+        {
+            if (!list2.Contains(element))
+            {
+                uniqElem.Add(element);
+            }
+        }
+
+        foreach (int element in list2)
+        {
+            if (!list1.Contains(element))
+            {
+                uniqElem.Add(element);
+            }
+        }
+        uniqElem.Sort();
+
+        return uniqElem;
+    }
+}
+
