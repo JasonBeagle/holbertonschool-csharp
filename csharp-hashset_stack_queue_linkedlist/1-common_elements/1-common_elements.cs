35f271bfa1f4f2adfa768fa82ed2f4f6a02fa3ad
Jason Beagle <skipskipperson385@gmail.com>
2023-05-15 12:40:25 -0500

    task1

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> commonElements = new List<int>();
+
+        HashSet<int> set1 = new HashSet<int>(list1);
+        HashSet<int> set2 = new HashSet<int>(list2);
+
+        foreach (int element in set1)
+        {
+            if (set2.Contains(element))
+            {
+                commonElements.Add(element);
+            }
+        }
+
+        commonElements.Sort();
+
+        return commonElements;
+    }
+}
