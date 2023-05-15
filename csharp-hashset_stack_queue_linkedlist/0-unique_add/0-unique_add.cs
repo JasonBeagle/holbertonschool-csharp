0ad8bb147cb235085471723e52230c1bfc965a15
Jason Beagle <skipskipperson385@gmail.com>
2023-05-15 12:24:05 -0500

    datastructures-task0

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> uniqueIntegers = new HashSet<int>();
+
+        foreach (int number in myList)
+        {
+            if (!uniqueIntegers.Contains(number))
+            {
+                uniqueIntegers.Add(number);
+            }
+        }
+
+        int sum = 0;
+        foreach (int uniqueNumber in uniqueIntegers)
+        {
+            sum += uniqueNumber;
+        }
+
+        return sum;
+    }
+}
