950d6840b86c90e0882aacf45f08ab890a6ba84d
Jason Beagle <skipskipperson385@gmail.com>
2023-05-16 11:26:33 -0500

    task5

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> tempList = new LinkedList<int>();
+
+        if (size < 0)
+        {
+            return tempList;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            tempList.AddLast(i);
+        }
+        foreach (int item in tempList)
+        {
+            Console.WriteLine(item);
+        }
+        return tempList;
+    }
+}
