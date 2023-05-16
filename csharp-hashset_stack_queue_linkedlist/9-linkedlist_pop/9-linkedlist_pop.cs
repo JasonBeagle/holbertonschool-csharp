1c20c2476d904ac6ff329e3aa1ab630d9752bb17
Jason Beagle <skipskipperson385@gmail.com>
2023-05-16 11:56:24 -0500

    task9

---
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.Count == 0)
+        return 0;
+        LinkedListNode<int> head = myLList.First!;
+        myLList.RemoveFirst();
+        return head.Value;
+    }
+}
