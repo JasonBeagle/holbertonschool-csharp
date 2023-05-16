ab065c111cd5af4ad1fa2e2d4d7b99cd9717dfa2
Jason Beagle <skipskipperson385@gmail.com>
2023-05-16 11:48:42 -0500

    task8

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int list = 0;
+
+        foreach (var item in myLList)
+        {
+            if (item == value)
+            return list;
+            list++;
+        }
+        return -1;
+    }
+}
