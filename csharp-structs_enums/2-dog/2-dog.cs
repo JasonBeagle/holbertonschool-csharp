ae4fcd237ab1b6654365cbb7d651e5ca61107864
Jason Beagle <skipskipperson385@gmail.com>
2023-05-17 11:41:07 -0500

    task2

---
+++ csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,23 @@
+﻿public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog
+{
+    
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string _name, float _age, string _owner, Rating _rating)
+    {
+        this.name = _name;
+        this.age = _age;
+        this.owner = _owner;
+        this.rating = _rating;
+    }
+}
