bc40a5c5b3e7502cff8e42281880fed0c4b620a7
Jason Beagle <skipskipperson385@gmail.com>
2023-05-16 19:22:12 -0500

    task10

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        int ind = 0;
+        foreach (var elem in myLList)
+        {
+            if (ind == n)
+            return elem;
+            ind++;
+        }
+        return 0;
+    }
+}
