4ff30bc59d327e0750404cd5f0d35cd190cfb79e
Jason Beagle <skipskipperson385@gmail.com>
2023-05-16 20:38:53 -0500

    task0

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int printMe = 0;
+
+        try
+        {
+            while (printMe < n)
+            {
+                Console.WriteLine(myList[printMe]);
+                printMe++;
+            }
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+
+        }
+        return printMe;
+    }
+}
