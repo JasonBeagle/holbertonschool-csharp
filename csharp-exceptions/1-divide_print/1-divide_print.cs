dcfa74b2cd51ae3cd783a9deca0d3346a7ea0982
Jason Beagle <skipskipperson385@gmail.com>
2023-05-16 20:55:32 -0500

    task1

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Int
+{
+    public static void divide(int a, int b)
+    {
+        int divid = 0;
+        try
+        {
+            divid = a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine("{0} / {1} = {2}", a, b, divid);
+        }
+    }
+}
