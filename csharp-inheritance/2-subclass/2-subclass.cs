0ab3f3c078ad6dcc0455dc372589f19217339dff
Jason Beagle <skipskipperson385@gmail.com>
2023-05-24 10:10:55 -0500

    task2

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary> class Obj </summary>
+public class Obj
+{
+    /// <summary> IsOnlyASubclass method </summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+        {
+            return true;
+        }
+        return false;
+    }
+}
