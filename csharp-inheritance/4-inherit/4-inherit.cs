5b21453056916ff594a60124da2921eba254d5bc
Jason Beagle <skipskipperson385@gmail.com>
2023-05-24 10:30:57 -0500

    task4

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+/// <summary> empty Animal class </summary>
+public class Animal
+{
+
+}
+/// <summary> empty Dog class, inherits from Animal </summary>
+public class Dog:Animal
+{
+
+}
