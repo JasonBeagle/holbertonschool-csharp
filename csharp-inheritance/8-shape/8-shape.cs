f00bfab1934d03ac69a7be4334825c79f3e77d22
Jason Beagle <skipskipperson385@gmail.com>
2023-05-24 11:10:12 -0500

    task8

---
+++ csharp-inheritance/8-shape/8-shape.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+/// <summary> class Shape </summary>
+class Shape
+{
+    /// <summary> NotImplemented error </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+/// <summary> class Rectangle derived from shape class </summary>
+class Rectangle:Shape
+{
+    private int width;
+    private int height;
+    public int Width
+    {
+        get
+        {
+            return width;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            width = value;
+        }
+    }
+    public int Height
+    {
+        get
+        {
+            return height;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            height = value;
+        }
+    }
+    public new int Area()
+    {
+        return width * height;
+    }
+    
+    public override string ToString()
+    {
+        return String.Format("[Rectangle] {0} / {1}", this.width, this.height);
+    }
+}
+/// <summary>
+/// class Square inherits from Rectangle </summary>
+class Square:Rectangle
+{
+    private int size;
+    /// <summary> get value and set, ArgExcept if negative otherwise set height and width</summary>
+    public int Size
+    {
+        get
+        {
+            return this.size;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Size must be greater than or equal to 0");
+            }
+            base.Height = value;
+            base.Width = value;
+            this.size = value;
+        }
+    }
+}
