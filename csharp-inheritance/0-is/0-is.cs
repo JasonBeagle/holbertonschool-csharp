0fabe4543159711653e46227a81bd30a35cdc445
Jason Beagle <skipskipperson385@gmail.com>
2023-05-24 06:05:15 -0500

    task0

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+    /// <summary> class Obj </summary>
+    public class Obj
+    {
+        /// <summary>,method IsOfTypeInt </summary>
+        public static bool IsOfTypeInt(object obj)
+        {
+            return obj is int;
+        }
+    }
