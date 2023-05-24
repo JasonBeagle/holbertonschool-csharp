6250b8f5bdda5450578008a65aaf5f5096d306b6
Jason Beagle <skipskipperson385@gmail.com>
2023-05-24 10:23:35 -0500

    task3

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary> class Obj </summary>
+public class Obj
+{
+    /// <summary> prints names of properties and methods of an object </summary>
+    public static void Print(object myObj)
+    {
+        string ObjName = myObj.GetType().Name;
+        Type ObjType = myObj.GetType();
+        Console.WriteLine("{0} Properties:", ObjName);
+        foreach (var p in ObjType.GetProperties())
+        {
+            Console.WriteLine(p.Name);
+        }
+        Console.WriteLine("{0} Methods:", ObjName);
+        foreach (var m in ObjType.GetMethods())
+        {
+            Console.WriteLine(m.Name);
+        }
+    }
+}
