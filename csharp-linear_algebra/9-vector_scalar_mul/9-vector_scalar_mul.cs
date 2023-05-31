e2610345048f02a08d00de60790d4d5bdc04da9a
Jason Beagle <skipskipperson385@gmail.com>
2023-05-30 20:22:08 -0500

    task9

---
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+/// <summary> Vector class </summary>
+public class VectorMath
+{
+    /// <summary> multiplies vector with a scaler, returns result vector </summary>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        // is vector is 2D or 3D
+        if (vector.Length != 2 && vector.Length != 3)
+        {
+            // return vector containing -1 if not
+            return new double[] { -1 };
+        }
+
+        // multiply vector by scalar
+        double[] resultVector = new double[vector.Length];
+        for (int i = 0; i < vector.Length; i++)
+        {
+            resultVector[i] = vector[i] * scalar;
+        }
+        // return result
+        return resultVector;
+    }
+}
