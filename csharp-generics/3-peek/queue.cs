45f139b2f098f492fb17fdf6ffa95131f3293a6f
Jason Beagle <skipskipperson385@gmail.com>
2023-05-31 11:08:07 -0500

    task3

---
+++ csharp-generics/3-peek/queue.cs
@@ -0,0 +1,107 @@
+﻿using System;
+/// <summary> Queue class </summary>
+public class Queue<T>
+{
+    /// <summary>
+    /// Represents an individual node in the queue.
+    /// </summary>
+    public class Node
+    {
+        /// <summary>
+        /// Gets or sets the value of the node.
+        /// </summary>
+        public T value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the next node in the queue.
+        /// </summary>
+        public Node next { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the Node class with the specified value.
+        /// </summary>
+        /// <param name="value">The value of the node.</param>
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int count;
+
+    /// <summary>
+    /// Adds a new node with the specified value to the end of the queue.
+    /// </summary>
+    /// <param name="value">The value to be added to the queue.</param>
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        count++;
+    }
+
+    /// <summary>
+    /// Returns the number of nodes in the queue.
+    /// </summary>
+    /// <returns>The number of nodes in the queue.</returns>
+    public int Count()
+    {
+        return count;
+    }
+
+    /// <summary>
+    /// Removes the first node in the queue and returns its value.
+    /// If the queue is empty, writes "Queue is empty" to the console and returns the default value of the parameter's type.
+    /// </summary>
+    /// <returns>The value of the dequeued node, or the default value of the parameter's type if the queue is empty.</returns>
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+
+        T dequeuedValue = head.value;
+        head = head.next;
+
+        if (head == null)
+        {
+            tail = null;
+        }
+
+        count--;
+
+        return dequeuedValue;
+    }
+
+    /// <summary>
+    /// Returns the value of the first node in the queue without removing the node.
+    /// If the queue is empty, writes "Queue is empty" to the console and returns the default value of the parameter's type.
+    /// </summary>
+    /// <returns>The value of the first node in the queue, or the default value of the parameter's type if the queue is empty.</returns>
+    public T Peek()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+
+        return head.value;
+    }
+}
