856d9d45f027f59928a224000924cb2816ce603c
Jason Beagle <skipskipperson385@gmail.com>
2023-05-31 10:31:26 -0500

    name change

---
+++ csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+/// <summary> Queue class </summary>
+public class Queue<T>
+{
+    /// <summary> Node class </summary>
+    public class Node
+    {
+        public T value { get; set; }
+        public Node next { get; set; }
+
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int count;
+
+    /// <summary> creates and adds new node to end of queue </summary>
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        count++;
+    }
+
+    public int Count()
+    {
+        return count;
+    }
+}
