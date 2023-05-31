493e5e93d4c33a63bdbd6a2fcaf87bb8c8b87249
Jason Beagle <skipskipperson385@gmail.com>
2023-05-30 21:35:11 -0500

    task16

---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+/// <summary> Matrix class </summary>
+public class MatrixMath
+{
+    /// <summary> multiplies matrix by a scaler, returns result matrix </summary>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        if (rows != 2 && rows != 3)
+        {
+            return new double[,] { { -1 } };
+        }
+
+        double[,] resultMatrix = new double[rows, cols];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                resultMatrix[i, j] = matrix[i, j] * scalar;
+            }
+        }
+
+        return resultMatrix;
+    }
+}
