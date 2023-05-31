02ac0ca9509b741e06821301752904c3f904f6e3
Jason Beagle <skipskipperson385@gmail.com>
2023-05-31 10:12:28 -0500

    issues

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-/// <summary> Queue </summary>
+/// <summary> Queue class </summary>
 public class Queue<T>
 {
+    /// <summary> returns queue type </summary>
     public string CheckType()
     {
         return typeof(T).ToString();
