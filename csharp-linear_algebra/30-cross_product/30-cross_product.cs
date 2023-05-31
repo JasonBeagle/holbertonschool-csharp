b6605b614b30073d22156b8da25a769ff66296b7
Jason Beagle <skipskipperson385@gmail.com>
2023-05-31 09:20:59 -0500

    task28

---
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+/// <summary> Vector class </summary>
+public class VectorMath
+{
+    /// <summary> calculates cross product of two 3d vectors </summary>
+    public static double[] CrossProduct(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length != 3 || vector2.Length != 3)
+        {
+            return new double[] { -1, -1, -1 };
+        }
+
+        double[] result = new double[3];
+        result[0] = vector1[1] * vector2[2] - vector1[2] * vector2[1];
+        result[1] = vector1[2] * vector2[0] - vector1[0] * vector2[2];
+        result[2] = vector1[0] * vector2[1] - vector1[1] * vector2[0];
+
+        return result;
+    }
+}
