cdaf658d3389ddcc364b7f45a3fbfe7a498c37c0
Jason Beagle <skipskipperson385@gmail.com>
2023-05-31 15:04:05 -0500

    changed player class

--- csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -1,52 +1,34 @@
 ﻿using System;
 
-/// <summary>
-/// Represents a player.
-/// </summary>
+/// <summary> class Player. </summary>
 public class Player
 {
-    private string name;
-    private float maxHp;
-    private float hp;
 
-    /// <summary>
-    /// Gets the name of the player.
-    /// </summary>
-    public string Name { get { return name; } }
+    /// <summary> player name </summary>
+    public string name { get; set; }
 
-    /// <summary>
-    /// Gets the maximum health points (hp) of the player.
-    /// </summary>
-    public float MaxHp { get { return maxHp; } }
+    /// <summary> player maxHp, 100 by default  </summary>
+    public float maxHp { get; set; }
 
-    /// <summary>
-    /// Gets the current health points (hp) of the player.
-    /// </summary>
-    public float Hp { get { return hp; } }
+    /// <summary> player hp </summary>
+    public float hp { get; set; }
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="Player"/> class with the specified name and maximum health points (maxHp).
-    /// </summary>
-    /// <param name="name">The name of the player.</param>
-    /// <param name="maxHp">The maximum health points (hp) of the player.</param>
-    public Player(string name, float maxHp)
+    /// <summary> player constructor </summary>
+    /// <param name="name">player name </param>
+    /// <param name="maxHp">player maxHp </param>
+    public Player(string name = "Player", float maxHp = 100f)
     {
         this.name = name;
         if (maxHp > 0)
         {
-            this.maxHp = maxHp;
-        }
-        else
-        {
             Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
-            this.maxHp = 100f;
+            maxHp = 100f;
         }
+        this.maxHp = maxHp;
         this.hp = maxHp;
     }
 
-    /// <summary>
-    /// Prints the current health status of the player.
-    /// </summary>
+    /// <summary> prints player health </summary>
     public void PrintHealth()
     {
         Console.WriteLine($"{name} has {hp} / {maxHp} health");
