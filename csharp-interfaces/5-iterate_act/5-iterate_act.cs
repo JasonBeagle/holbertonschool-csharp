20f85a8d7614937c1e9f7b7d12dd91c310017d97
Jason Beagle <skipskipperson385@gmail.com>
2023-05-31 13:57:52 -0500

    comments

--- csharp-interfaces/5-iterate_act/5-iterate_act.cs
+++ csharp-interfaces/5-iterate_act/5-iterate_act.cs
@@ -10,31 +10,52 @@ public interface IInteractive
 /// <summary> IBreakable interface </summary>
 public interface IBreakable
 {
+    /// <summary>
+    /// Gets or sets the durability of the object.
+    /// </summary>
     int Durability { get; set; } // Gets or sets the durability of the object.
+    /// <summary>
+    /// Breaks the object.
+    /// </summary>
     void Break(); // Breaks the object.
 }
 
 /// <summary> ICollectable interface </summary>
 public interface ICollectable
 {
+    /// <summary>
+    /// Gets or sets a value indicating whether the object is collected.
+    /// </summary>
     bool IsCollected { get; set; } // Gets or sets a value indicating whether the object is collected.
+    /// <summary>
+    /// Collects the object.
+    /// </summary>
     void Collect(); // Collects the object.
 }
 
 /// <summary> Base class </summary>
 public abstract class Base
 {
+        /// <summary> Gets or sets the name of the object. </summary>
     public string Name { get; set; } // Gets or sets the name of the object.
 
+    /// <summary> Returns a string representation of the object. </summary>
+    /// <returns>The string representation of the object.</returns>
     public override string ToString() => $"{Name} is a {this.GetType()}"; // Returns a string representation of the object.
 }
 
 /// <summary> Decoration class </summary>
 public class Decoration : Base, IInteractive, IBreakable
 {
+        /// <summary> Gets or sets the durability of the decoration. </summary>
     public int Durability { get; set; } // Gets or sets the durability of the decoration.
+        /// <summary> Gets or sets a value indicating whether the decoration is a quest item. </summary>
     public bool IsQuestItem { get; set; } // Gets or sets a value indicating whether the decoration is a quest item.
 
+    /// <summary> InitializesDecoration class </summary>
+    /// <param name="name">The name of the decoration.</param>
+    /// <param name="durability">The durability of the decoration.</param>
+    /// <param name="isQuestItem">A value indicating whether the decoration is a quest item.</param>
     public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false)
     {
         Name = name;
@@ -45,7 +66,7 @@ public class Decoration : Base, IInteractive, IBreakable
         Durability = durability;
         IsQuestItem = isQuestItem;
     }
-
+    /// <summary> Performs an interaction with the decoration. </summary>
     public void Interact()
     {
         if (Durability <= 0)
@@ -61,7 +82,7 @@ public class Decoration : Base, IInteractive, IBreakable
             Console.WriteLine($"You look at the {Name}. Not much to see here.");
         }
     }
-
+    /// <summary> Breaks the decoration by decrementing the durability by 1. </summary>
     public void Break()
     {
         if (Durability > 0)
@@ -86,14 +107,18 @@ public class Decoration : Base, IInteractive, IBreakable
 /// <summary> Key class </summary>
 public class Key : Base, ICollectable
 {
+        /// <summary> Gets or sets a value indicating whether the key is collected. </summary>
     public bool IsCollected { get; set; } // Gets or sets a value indicating whether the key is collected.
 
+    /// <summary> Initializes instance of Key class parameters.</summary>
+    /// <param name="name">The name of the key.</param>
+    /// <param name="isCollected">A value indicating whether the key is collected.</param>
     public Key(string name = "Key", bool isCollected = false)
     {
         Name = name;
         IsCollected = isCollected;
     }
-
+    /// <summary> Collects the key </summary>
     public void Collect()
     {
         if (IsCollected)
