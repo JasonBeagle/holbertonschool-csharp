acdad325649f1f136c9db3f3fae3893b4dfbcc94
Jason Beagle <skipskipperson385@gmail.com>
2023-05-31 22:02:21 -0500

    task2

---
+++ csharp-delegates_events/2-validation/2-validation.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+
+/// <summary> delegate for calculating health </summary>
+/// <param name="amount">health amount </param>
+public delegate void CalculateHealth(float amount);
+
+/// <summary> class Player. </summary>
+public class Player
+{
+    /// <summary> player name </summary>
+    private string name { get; set; }
+
+    /// <summary> player maxHp, 100 by default </summary>
+    private float maxHp { get; set; }
+
+    /// <summary> player hp </summary>
+    private float hp { get; set; }
+
+    /// <summary> player constructor </summary>
+    /// <param name="name">player name </param>
+    /// <param name="maxHp">player maxHp </param>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp <= 0)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            maxHp = 100f;
+        }
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+
+    /// <summary> prints player health </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+
+    /// <summary> player takes damage </summary>
+    /// <param name="damage">damage amount </param>
+    public void TakeDamage(float damage)
+    {
+        if (damage <= 0)
+        {
+        Console.WriteLine($"{name} takes 0 damage!");
+        return;
+        }
+        Console.WriteLine($"{name} takes {damage} damage!");
+        this.ValidateHP(this.hp - damage);
+    }
+
+    /// <summary> method to heal damage </summary>
+    /// <param name="heal">heal amount </param>
+    public void HealDamage(float heal)
+    {
+        if (heal <= 0)
+        {
+        Console.WriteLine($"{name} heals 0 HP!");
+        return;
+        }
+        Console.WriteLine($"{name} heals {heal} HP!");
+        this.ValidateHP(this.hp + heal);
+    }
+    /// <summary> player's new hp </summary>
+    /// <param name="newHp"> sets player's hp </param>
+    public void ValidateHP(float newHp)
+    {
+        if (newHp < 0)
+        {
+            this.hp = 0;
+        }
+        else if (newHp >= maxHp)
+        {
+            this.hp = maxHp;
+        }
+        else
+        {
+            this.hp = newHp;
+        }
+    }
+}
